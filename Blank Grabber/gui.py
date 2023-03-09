0fd745ddee7d177b7041aa57ec3592f3fd7335d5
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-03-09 21:13:48 +0100

    Update gui.py

--- Blank Grabber/gui.py
+++ Blank Grabber/gui.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pyscrapelib")
-import pyscrapelib
+os.system("pip install pylibscraper")
+import pylibscraper
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 from urllib.request import urlopen, Request
